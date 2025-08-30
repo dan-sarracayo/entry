fb4dbe2f9c4f40686abb3d486e2ce575d7c52e66
Dan Sarracayo <daniel.sarracayo@gmail.com>
2025-08-30 09:11:39 +0100

    Finished install script.

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-ENTRY_REPO_URL="";
-ENTRY_SCRIPT_URL="";
-ENTRY_CONFIG_URL="";
+ENTRY_REPO_URL="https://raw.githubusercontent.com/dan-sarracayo/entry/refs/heads/main";
+ENTRY_SCRIPT_URL="$ENTRY_REPO_URL/entry";
+ENTRY_CONFIG_URL="$ENTRY_REPO_URL/entry.config";
+
+LOCAL_BIN="$HOME/.local/bin";
+LOCAL_CONFIG="$HOME/.config";
+
+# Check/create dir.
+function checkdir {
+	if [ ! -d "$1" ]; then
+		read -p "[info] create a $1 directory? (y)Yes/(n)No/:- " choice;
+		if [ $choice == "y" ]; then
+			mkdir -p "$1";
+		else
+			echo "[info] cancelling...";
+			exit;
+		fi
+	else
+		echo "[info] $1 detected.";
+	fi
+}
 
 # Install script.
-if [ ! -d "$HOME/.local/bin" ]; then
-	read -p "Create a ~/.local/bin directory? (y)Yes/(n)No/:- " choice;
-	if [ $choice == "y" ]; then
-		mkdir -p "$HOME/.local/bin";
-	else
-			echo "Cancelling...";
-			exit;
-	fi
-
-	wget $ENTRY_SCRIPT_URL ~/.local/bin/;
-fi
+checkdir "$LOCAL_BIN";
+wget $ENTRY_SCRIPT_URL -q -O "$LOCAL_BIN/entry";
+echo "[info] installed script."
 
 # Install config.
-if [ ! -d "$HOME/.config" ]; then 
-	read -p "Create a ~/.config directory? (y)Yes/(n)No/:- " choice;
-	if [ $choice == "y" ]; then
-		mkdir -p "$HOME/.config";
-	else
-			echo "Cancelling...";
-			exit;
-	fi
-	
-	wget $ENTRY_CONFIG_URL ~/.local/bin/;
-fi
+checkdir "$LOCAL_CONFIG";
+wget $ENTRY_CONFIG_URL -q -O "$LOCAL_CONFIG/entry.config";
+echo "[info] installed config."
+
